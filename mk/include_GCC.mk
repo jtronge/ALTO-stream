9c6baadb03df3ecd0727c9035ae406b2b2bb7895
Jake Tronge <jtronge3@gmail.com>
2026-03-15 11:51:13 -0400

    Remove -march=native due to missing vmovw instruction

--- mk/include_GCC.mk
+++ mk/include_GCC.mk
@@ -21,7 +21,7 @@ ifeq ($(DEBUG),true)
 CXXFLAGS += $(OPENMP) $(BLASCFLAGS) -O0 -static-libasan -O -g -fsanitize=address -fno-omit-frame-pointer -march=native -static -Wall -g -std=c++17 -D _GLIBCXX_PARALLEL
 LIBS	 = -static-libasan -O -g -fsanitize=address -fno-omit-frame-pointer -lpthread -lm -ldl $(BLASLIBS)
 else
-CXXFLAGS += $(OPENMP) $(BLASCFLAGS) -O3 -march=native -static -g -std=c++17 -D_GLIBCXX_PARALLEL
+CXXFLAGS += $(OPENMP) $(BLASCFLAGS) -O3 -static -g -std=c++17 -D_GLIBCXX_PARALLEL
 LIBS	 = -Wl,--no-as-needed -lpthread -lm -ldl $(BLASLIBS)
 endif
 
