2e62594154ca91e9e21f0cfb44dfff4accf1c7b2
Jake Tronge <jtronge3@gmail.com>
2026-03-15 10:20:21 -0400

    Update wrapper code for external usage

--- include/alto_wrapper.hpp
+++ include/alto_wrapper.hpp
@@ -5,17 +5,15 @@ namespace alto {
 
 class AltoTensorWrapper {
 public:
-    AltoTensorWrapper();
+    AltoTensorWrapper(int nmodes, uint64_t* dims, uint64_t nnz,
+                      uint64_t** ind, double* vals, int nthreads);
     ~AltoTensorWrapper();
+    void run_mttkrp(int mode, double** factor_mats, uint64_t rank);
 
 private:
     struct Pimpl;
     std::unique_ptr<Pimpl> pimpl_;
 };
-
-std::unique_ptr<AltoTensorWrapper> make_alto_tensor(int nmodes, uint64_t* dims, uint64_t nnz, uint64_t** ind, double* vals, int nthreads);
-
-void run_mttkrp(int mode, double** factor_mats, uint64_t rank, void* alto_tensor);
 
 }
 
--- src/wrapper.cpp
+++ src/wrapper.cpp
@@ -31,46 +31,64 @@
 
 namespace alto {
 
-AltoTensorWrapper::AltoTensorWrapper()
+struct AltoTensorWrapper::Pimpl {
+    AltoTensor<LIType>* at;
+    std::vector<FType*> factor_ptrs;
+
+    Pimpl(int nmodes, uint64_t* dims, uint64_t nnz, uint64_t** ind, double* vals, int nthreads)
+    {
+        if (sizeof(FType) != sizeof(double)) {
+            throw std::runtime_error{"ALTO Ftype is not same as double type"};
+        }
+
+        SparseTensor* spt = AllocSparseTensor(nnz, nmodes);
+        printf("generating alto sparse tensor\n");
+        for (int m = 0; m < nmodes; ++m) {
+            spt->dims[m] = dims[m];
+            for (uint64_t n = 0; n < nnz; ++n) {
+                spt->cidx[m][n] = ind[m][n];
+            }
+            factor_ptrs.push_back(nullptr);
+        }
+        for (uint64_t n = 0; n < nnz; ++n) {
+            spt->vals[n] = vals[n];
+        }
+
+        // -1 set to skip vectorized streaming mode optimizations
+        init_salto(spt, &at, nthreads, -1);
+        // TODO: May want to check best number of partitions
+        int num_partitions = get_num_ptrn(nthreads);
+        update_salto(spt, at, num_partitions);
+        DestroySparseTensor(spt);
+    }
+
+    ~Pimpl()
+    {
+        destroy_alto(at);
+    }
+
+    void run_mttkrp(int mode, double** factor_mats, uint64_t rank)
+    {
+        for (uint64_t m = 0; m < factor_ptrs.size(); ++m) {
+            factor_ptrs[m] = factor_mats[m];
+        }
+        mttkrp_alto_atomic(mode, factor_ptrs.data(), at, factor_ptrs.size(), rank);
+    }
+};
+
+AltoTensorWrapper::AltoTensorWrapper(int nmodes, uint64_t* dims, uint64_t nnz,
+                                     uint64_t** ind, double* vals, int nthreads)
 {
+    pimpl_ = std::make_unique<AltoTensorWrapper::Pimpl>(nmodes, dims, nnz, ind, vals, nthreads);
 }
 
 AltoTensorWrapper::~AltoTensorWrapper()
 {
 }
 
-struct AltoTensorWrapper::Pimpl {
-    // TODO
-    int i;
-};
-
-std::unique_ptr<AltoTensorWrapper> make_alto_tensor(int nmodes, uint64_t* dims, uint64_t nnz,
-                                                    uint64_t** ind, double* vals, int nthreads)
+void AltoTensorWrapper::run_mttkrp(int mode, double** factor_mats, uint64_t rank)
 {
-    SparseTensor* spt = AllocSparseTensor(nnz, nmodes);
-    printf("generating alto sparse tensor\n");
-    for (int m = 0; m < nmodes; ++m) {
-        spt->dims[m] = dims[m];
-        for (uint64_t n = 0; n < nnz; ++n) {
-            spt->cidx[m][n] = ind[m][n];
-        }
-    }
-    for (uint64_t n = 0; n < nnz; ++n) {
-        spt->vals[n] = vals[n];
-    }
-
-    AltoTensor<LIType>* at;
-
-    // -1 set to skip vectorized streaming mode optimizations
-    init_salto(spt, &at, nthreads, -1);
-    // TODO: May want to check best number of partitions
-    int num_partitions = get_num_ptrn(nthreads);
-    update_salto(spt, at, num_partitions);
-    DestroySparseTensor(spt);
-
-    destroy_alto(at);
-
-    return nullptr;
+    pimpl_->run_mttkrp(mode, factor_mats, rank);
 }
 
 }
